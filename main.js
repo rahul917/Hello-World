8779229da41f2ed327e6b11cee0c9d8af300cf6f
Rahul Agrawal <RahulAgrawal917@yahoo.com>
2017-03-22 12:57:43 -0400

    practicing jquery

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-<script>
+	function SetEven() {
+		$("li, span").css("font-weight","");
+		$("li:even").css("font-weight","bold");
+		$(".label").css("font-weight", "bold");
+		$(".label").html("Even Planets");
+	}
+	
+	function SetOdd() {
+		$("li, span").css("font-weight","");
+		$("li:odd").css("font-weight","bold");
+		$(".label").css("font-weight", "bold");
+		$(".label").html("Odd Planets");
+	}
+	
+	function SetFirst(num) {
+		
+		if(typeof(num) == "undefined")
+			num = 6;
+		$("li, span").css("font-weight","");
+		$("li:lt(4)").css("font-weight","bold");
+		$(".label").css("font-weight", "bold");
+		$(".label").html("First 4 Planets");
+	}
+	
+	function SetLast() {
+		
+		$("li, span").css("font-weight","");
+		$("li:gt(5)").css("font-weight","bold");
+		$(".label").css("font-weight", "bold");
+		$(".label").html("Last 2 Planets");
+	}
 
-</script>
